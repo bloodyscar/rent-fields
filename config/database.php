578db8b6e9b407e839f1b756cf8b53fe1005e8fd
Adit <adithernowo@gmail.com>
2024-11-05 13:56:54 +0700

    update

--- config/database.php
+++ config/database.php
@@ -60,9 +60,9 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-            'options' => array(
-                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET time_zone = \'+00:00\''
-            )
+            // 'options' => array(
+            //     \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET time_zone = \'+00:00\''
+            // )
         ],
 
         'mariadb' => [
